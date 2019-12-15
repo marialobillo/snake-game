c623acaf3a570e6073478d105cdb853de28249a7
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2019-12-15 11:20:57 +0100

    showing apple

--- app.js
+++ app.js
@@ -1,64 +1,85 @@
 (function(){
 
     const canvas = document.querySelector('#board');
-const context = canvas.getContext('2d');
-const scale = 15;
+    const context = canvas.getContext('2d');
+    const scale = 15;
+    const rows = canvas.height / scale;
+    const columns = canvas.width / scale;
 
-let x = 0;
-let y = 0;
-let xSpeed = scale * 1;
-let ySpeed = 0;
-let width = 15;
-let height = 15;
+    let x = 0;
+    let y = 0;
+    let xSpeed = scale * 1;
+    let ySpeed = 0;
+    let width = 15;
+    let height = 15;
+    let appleX, appleY;
 
-context.rect(x, y, width, height);
-context.stroke();
+    context.rect(x, y, width, height);
+    context.stroke();
 
 
-function move(e){
+    function move(e){
 
-    switch(e.keyCode){
-        case 37:
-            xSpeed = -scale * 1;
-            ySpeed = 0; 
-            break;
-        case 38:
-            xSpeed = 0;
-            ySpeed = -scale * 1;
-            break; 
-        case 39:
-            xSpeed = scale * 1;
-            ySpeed = 0;
-            break;
-        case 40:
-            xSpeed = 0;
-            ySpeed = scale * 1;
-            break;
+        switch(e.keyCode){
+            case 37:
+                xSpeed = -scale * 1;
+                ySpeed = 0; 
+                break;
+            case 38:
+                xSpeed = 0;
+                ySpeed = -scale * 1;
+                break; 
+            case 39:
+                xSpeed = scale * 1;
+                ySpeed = 0;
+                break;
+            case 40:
+                xSpeed = 0;
+                ySpeed = scale * 1;
+                break;
+        }
     }
 
-    
-    
-}
+    function update(){
+        x += xSpeed;
+        y += ySpeed;
+    }
 
-function update(){
-    x += xSpeed;
-    y += ySpeed;
-}
+    function drawSnake(){
+        context.fillStyle = '#FFFFFF';
+        context.fillRect(x, y, scale, scale);
+    }
 
-function draw(){
-    context.fillStyle = '#FFFFFF';
-    context.fillRect(x, y, scale, scale);
-}
+    function getApplePosition(){
+         // randomly x and y
+         appleX = (Math.floor(Math.random() * rows - 1) + 1) * scale;
+         appleY = (Math.floor(Math.random() * columns - 1) + 1) * scale;
+    }
+    function drawApple(){
+       
 
-document.onkeydown = move;
+        context.fillStyle = '#FF0000';
+        context.fillRect(appleX, appleY, scale, scale);
+    }
 
-window.setInterval(() => {
-    context.clearRect(0,0, canvas.width, canvas.height);
-    update();
-    draw();
-}, 250);
+    function main(){
+        document.onkeydown = move;
+
+        getApplePosition();
 
 
+        window.setInterval(() => {
+            context.clearRect(0,0, canvas.width, canvas.height);
+            drawApple();
+            update();
+            drawSnake();
 
+
+            
+
+        }, 250);
+    }
+
+    main();
 
 })();
